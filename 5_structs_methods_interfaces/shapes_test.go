0a7597986d2bc8e7486f0013e0398719adbdb363
franco <francojposa@gmail.com>
2019-11-10 09:33:34 -0600

    Structs satisfying interfaces, testing against the interface-promised behavior

--- 5_structs_methods_interfaces/shapes_test.go
+++ 5_structs_methods_interfaces/shapes_test.go
@@ -15,11 +15,25 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{5.0, 8.0}
-	want := 40.0
-	got := Area(rectangle)
 
-	if got != want {
-		t.Errorf("\nwant: %.2f, got: %.2f", want, got)
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("\nwant: %.2f, got: %.2f", want, got)
+		}
 	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{5.0, 8.0}
+		want := 40.0
+		checkArea(t, rectangle, want)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10.0}
+		want := 314.1592653589793
+		checkArea(t, circle, want)
+	})
+
 }
