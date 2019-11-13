ff871cc635ec61e48f6ad3e4e7c972209c6fdd37
francojposa <francojposa@gmail.com>
2019-11-13 14:41:27 -0600

    Adding a third struct that satisfies the Shape interface, using table-driven test with anonymous struct

--- 5_structs_methods_interfaces/shapes_test.go
+++ 5_structs_methods_interfaces/shapes_test.go
@@ -37,3 +37,21 @@ func TestArea(t *testing.T) {
 	})
 
 }
+
+func TestAreaWithTables(t *testing.T) {
+
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+	}
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("\nwant: %g, got: %g", tt.want, got)
+		}
+	}
+}
