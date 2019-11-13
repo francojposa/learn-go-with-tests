74bb5fe6dbb01cdd05ab92efa1e28e958a288521
francojposa <francojposa@gmail.com>
2019-11-13 15:06:22 -0600

    Using anonymouse struct name in t.Tun to name our table-driven tests; using #v to print the contents of the tested object

--- 5_structs_methods_interfaces/shapes_test.go
+++ 5_structs_methods_interfaces/shapes_test.go
@@ -41,17 +41,22 @@ func TestArea(t *testing.T) {
 func TestAreaWithTables(t *testing.T) {
 
 	areaTests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
-		{shape: Circle{Radius: 10}, want: 314.1592653589793},
-		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("\nwant: %g, got: %g", tt.want, got)
-		}
+		// using tt.name from the case to use it as the t.Run test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("\n%#v want: %.2f, got: %.2f", tt.shape, tt.want, got)
+			}
+		})
+
 	}
 }
