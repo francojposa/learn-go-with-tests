a01c79dd40f0a8869450c1708abd8be14550c893
francojposa <francojposa@gmail.com>
2020-03-05 09:49:48 -0600

    Updating in memory store to store the full pointer objects instead of creating them

--- application/in_memory_player_repo.go
+++ application/in_memory_player_repo.go
@@ -4,13 +4,13 @@ import "sync"
 
 func NewInMemoryPlayerRepo() *InMemoryPlayerStore {
 	return &InMemoryPlayerStore{
-		map[string]int{},
+		map[string]*PlayerScore{},
 		sync.RWMutex{},
 	}
 }
 
 type InMemoryPlayerStore struct {
-	scores map[string]int
+	scores map[string]*PlayerScore
 	// A mutex is used to synchronize read/write access to the map
 	lock sync.RWMutex
 }
@@ -18,27 +18,31 @@ type InMemoryPlayerStore struct {
 func (i *InMemoryPlayerStore) GetPlayerScore(id string) (*PlayerScore, bool) {
 	i.lock.RLock()
 	defer i.lock.RUnlock()
-	score, ok := i.scores[id]
+	playerScore, ok := i.scores[id]
 	if ok {
-		return &PlayerScore{id, score}, ok
-	} else {
-		return nil, ok
+		return playerScore, ok
 	}
+	return nil, ok
 
 }
 
 func (i *InMemoryPlayerStore) ListPlayerScores() []*PlayerScore {
 	playerScores := []*PlayerScore{}
-	for playerId, score := range i.scores {
-		playerScores = append(playerScores, &PlayerScore{playerId, score})
+	for _, playerScore := range i.scores {
+		playerScores = append(playerScores, playerScore)
 	}
-
 	return playerScores
 }
 
 func (i *InMemoryPlayerStore) RecordPlayerScore(id string) *PlayerScore {
 	i.lock.Lock()
 	defer i.lock.Unlock()
-	i.scores[id]++
-	return &PlayerScore{id, i.scores[id]}
+	playerScore, ok := i.scores[id]
+	if ok {
+		playerScore.Score++
+		return playerScore
+	}
+	newPlayerScore := &PlayerScore{id, 1}
+	i.scores[id] = newPlayerScore
+	return newPlayerScore
 }
--- application/in_memory_player_repo_test.go
+++ application/in_memory_player_repo_test.go
@@ -8,9 +8,9 @@ import (
 
 func TestGetPlayerScore(t *testing.T) {
 	repo := &InMemoryPlayerStore{
-		map[string]int{
-			"1": 20,
-			"2": 10,
+		map[string]*PlayerScore{
+			"1": &PlayerScore{"1", 20},
+			"2": &PlayerScore{"2", 10},
 		},
 		sync.RWMutex{},
 	}
@@ -28,9 +28,9 @@ func TestListPlayerScores(t *testing.T) {
 
 	t.Run("list player scores for non-empty repo", func(t *testing.T) {
 		repo := &InMemoryPlayerStore{
-			map[string]int{
-				"1": 20,
-				"2": 10,
+			map[string]*PlayerScore{
+				"1": &PlayerScore{"1", 20},
+				"2": &PlayerScore{"2", 10},
 			},
 			sync.RWMutex{},
 		}
@@ -40,6 +40,44 @@ func TestListPlayerScores(t *testing.T) {
 		}
 		playerScores := repo.ListPlayerScores()
 		assertPlayerScoreSlice(t, expectedScores, playerScores)
+	})
+
+	t.Run("list player scores for empty repo", func(t *testing.T) {
+		repo := NewInMemoryPlayerRepo()
+		expectedScores := []*PlayerScore{}
+		playerScores := repo.ListPlayerScores()
+		assertPlayerScoreSlice(t, expectedScores, playerScores)
+	})
+
+}
+
+func TestRecordPlayerScore(t *testing.T) {
+	t.Run("create first player score for nonexistent player", func(t *testing.T) {
+		repo := NewInMemoryPlayerRepo()
+		expectedPlayer1Score := &PlayerScore{"1", 1}
+		player1Score := repo.RecordPlayerScore("1")
+		assertPlayerScore(t, expectedPlayer1Score, player1Score)
+	})
+
+	t.Run("increment player score for existing player", func(t *testing.T) {
+		repo := &InMemoryPlayerStore{
+			map[string]*PlayerScore{
+				"1": &PlayerScore{"1", 20},
+				"2": &PlayerScore{"2", 10},
+			},
+			sync.RWMutex{},
+		}
+
+		expectedPlayer1Score := &PlayerScore{"1", 22}
+		player1Score := repo.RecordPlayerScore("1")
+		repo.RecordPlayerScore("1")
+
+		assertPlayerScore(t, expectedPlayer1Score, player1Score)
+
+		expectedPlayer2Score := &PlayerScore{"2", 11}
+		player2Score := repo.RecordPlayerScore("2")
+
+		assertPlayerScore(t, expectedPlayer2Score, player2Score)
 	})
 
 }
