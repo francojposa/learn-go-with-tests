313146f812eb8822a91c33eeaab7ed089bac873a
francojposa <francojposa@gmail.com>
2020-03-05 08:59:44 -0600

    testing list player scores

--- application/in_memory_player_repo_test.go
+++ application/in_memory_player_repo_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"sync"
 	"testing"
 )
@@ -23,10 +24,30 @@ func TestGetPlayerScore(t *testing.T) {
 	assertNilPlayerScore(t, player3Score)
 }
 
+func TestListPlayerScores(t *testing.T) {
+
+	t.Run("list player scores for non-empty repo", func(t *testing.T) {
+		repo := &InMemoryPlayerStore{
+			map[string]int{
+				"1": 20,
+				"2": 10,
+			},
+			sync.RWMutex{},
+		}
+		expectedScores := []*PlayerScore{
+			&PlayerScore{"1", 20},
+			&PlayerScore{"2", 10},
+		}
+		playerScores := repo.ListPlayerScores()
+		assertPlayerScoreSlice(t, expectedScores, playerScores)
+	})
+
+}
+
 func assertPlayerScore(t *testing.T, want, got *PlayerScore) {
 	t.Helper()
 	if *got != *want {
-		t.Errorf("\nincorrect player score\nwant: %q, got %q", want, got)
+		t.Errorf("\nincorrect player score\nwant: %q, got: %q", want, got)
 	}
 }
 
@@ -34,5 +55,16 @@ func assertNilPlayerScore(t *testing.T, got *PlayerScore) {
 	t.Helper()
 	if got != nil {
 		t.Errorf("\nunexpected player score\nwant: nil, got: %q", got)
+	}
+}
+
+func assertPlayerScoreSlice(t *testing.T, want, got []*PlayerScore) {
+	t.Helper()
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf(
+			"\nincorrect slice of player scores\nwant: %q, got: %q",
+			want,
+			got,
+		)
 	}
 }
